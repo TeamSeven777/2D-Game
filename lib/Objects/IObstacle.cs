20c07d693459f6693705429c2039288f17f873e3
Letterman7171 <jpfriend2005@gmail.com>
2026-09-14 10:47:52 -0400

    Began Library File Structure Created Obstacle Interface for blocks / trees *Awaiting more classes for implementation* #6 #12

---
+++ lib/Objects/IObstacle.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Fourest_Seven.lib.Objects
+{
+    internal interface IObstacle
+    {
+        public Rectangle boundingBox { get; set; }
+        public float originPos { get; set; }
+
+        // Sprite Class not yet implemented, or something similar
+        public Texture2D sprite { get; set; }
+
+        public void Update(GameTime gameTime);
+
+    }
+}
---
+++ lib/Objects/Obstacle.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rectangle = Microsoft.Xna.Framework.Rectangle;
+
+namespace The_Fourest_Seven.lib.Objects
+{
+    internal class Obstacle : IObstacle
+    {
+        public Rectangle boundingBox { get; set; }
+        public float originPos { get; set; }
+        public Texture2D sprite { get; set; }
+
+        public Obstacle(Rectangle boundingBox, float originPos, Texture2D sprite)
+        {
+            this.originPos = originPos;
+            this.sprite = sprite;
+            this.boundingBox = boundingBox;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            //If player.boundingBox.getArea == this.boundingBox.getArea (?) do player.Pos.X / Y - distance to edge
+            //OR
+            //If player.boundingBox.getArea == this.boundingBox.getArea do clamp player.Pos.X && || Y
+            //OR (more likely)
+            //CommandCheckCollisions();
+        }
+    }
+}
