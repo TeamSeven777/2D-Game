ce5c43490f9484da4a96ebab39cf2139217a0171
zzenith95 <24weinet@gmail.com>
2026-09-15 18:42:39 -0400

    IPlayer, IController, ICommand, and a DefaultPlayer class. Ver. 0.1.

---
+++ 2D-Game/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_Game
+    {
+    public interface IPlayer
+{
+        void MoveLeft();
+        void MoveRight();
+        void MoveUp();
+        void MoveDown();
+
+
+}
+}
